29b88bfd39b5a13414d63ce85d6672da1cb5466e
anlige <zhanghuiguoanlige@126.com>
2014-08-06 18:20:40 +0800

    修改library的加载方式

---
+++ src/Mo/Library/Extend/net.js
@@ -0,0 +1,57 @@
+﻿/*
+'by anlige at www.9fn.net
+MoLibNet.IpToLong
+MoLibNet.LongToIp
+MoLibNet.InSameNetWork
+MoLibNet.IsIP
+*/
+var MoLibNet={
+	/****************************************************
+	'@DESCRIPTION:	convert ip string to long
+	'@PARAM:	d [String] : ip string
+	'@RETURN:	[Long] result
+	'****************************************************/
+	IpToLong:function(d){
+		var c=0;
+		d=""+d+"";
+		if(d.indexOf(".")<=0)return 0;
+		var a=d.split(".");
+		if(a.length!=4)return 0;
+		for(var b=0;b<4;b++){
+			if(isNaN(a[b])||parseInt(a[b])<0||parseInt(a[b])>255)return 0;
+			c+=parseInt(a[b])*Math.pow(256,3-b);
+		}
+		return c;
+	},
+	/****************************************************
+	'@DESCRIPTION:	convert long to ip string
+	'@PARAM:	b [Long] : data
+	'@RETURN:	[String] ip string
+	'****************************************************/
+	LongToIp:function(b){
+		if(isNaN(b))return "";
+		b=parseInt(b);
+		var a=new Array(4);
+		a[0]=b>>>24;
+		a[1]=(b<<8)>>>24;
+		a[2]=(b<<16)>>>24;
+		a[3]=(b<<24)>>>24;
+		return a.join(".");
+	},
+	/****************************************************
+	'@DESCRIPTION:	ensure ip is in a specific network
+	'@PARAM:	a [String] : network. eg: '192.168.1.1/255.255.255.0' or '192.168.1.1/24'
+	'@PARAM:	g [String] : ip need to ensure
+	'@PARAM:	f [Variant] : forget it;
+	'@RETURN:	[Boolean] true/false
+	'****************************************************/
+	InSameNetWork:function(a,g,f){if(f==undefined){f=g;if(a.indexOf("/")<=0)a+="/255.255.255.255";if(a.indexOf("/")==a.length){return false}var e=a.split("/");if(e.length!=2){return false}a=e[1];g=e[0]}if(!isNaN(a)){var b=parseInt(a);if(a<0||a>32){return false}var d=0;for(var c=1;c<=32;c++){if(c<=a){d+=Math.pow(2,32-c)}}a=this.LongToIp(d)}if(!this.IsIP(a)||!this.IsIP(g)||!this.IsIP(f)){return false}a=this.IpToLong(a);g=this.IpToLong(g);f=this.IpToLong(f);if((a&g)!=(a&f)){return false}return true},
+
+	/****************************************************
+	'@DESCRIPTION:	ensure ip is a real ip.
+	'@PARAM:	c [String] : ip string
+	'@RETURN:	[Boolean] true/false
+	'****************************************************/
+	IsIP:function(c){if(c.indexOf(".")<=0){return false}var a=c.split(".");if(a.length!=4){return false}for(var b=0;b<4;b++){if(a[b]==""||isNaN(a[b])||parseInt(a[b])<0||parseInt(a[b])>255){return false}}return true}
+};
+return MoLibNet;
---
+++ src/Mo/Library/Extend/xml.js
@@ -0,0 +1,263 @@
+﻿/****************************************************
+'@DESCRIPTION:	define MoLibXML object
+'****************************************************/
+function MoLibXML(dom){
+	this.DOM=dom||null;
+	this.ROOT = null;
+	if(this.DOM!=null){
+		this.ROOT = this.DOM.documentElement;
+	}
+	this.queue=[this.ROOT];
+	this.selectedNode=this.ROOT;
+	this.index=-1;
+}
+MoLibXML.filter = function(node,filter,index){
+	if(node==null)return null;
+	if(F.string.startWith(filter,"!") && node.nodeName!=filter.substr(1)) return node;
+	else if(F.string.startWith(filter,"@") && F.string.startWith(node.nodeName,filter.substr(1))) return node;
+	else if(filter==":odd" && index%2==0) return node;
+	else if(filter==":even" && index%2==1) return node;
+	else if(node.nodeName==filter) return node;
+	return null;
+};
+MoLibXML.Collection = function(){
+	this.length=0;
+	this.index=0;
+	this.push = function(value){this[(this.length++)]=value;};
+	this.next = function(){
+		return this[this.index++];
+	};
+	this.reset = function(){this.index=0;};
+	this.eof = function(){return this.index>=this.length;};
+	this.filter = function(tag){
+		var col = new MoLibXML.Collection();
+		for(var i=0;i<this.length;i++){
+			var node = MoLibXML.filter(this[i],tag,i);
+			if(node!=null)col.push(node);
+		}
+		return col;
+	};
+};
+MoLibXML.prototype.select = function(xpath,parent){
+	if(this.ROOT==null)return;
+	if(typeof xpath=="string"){
+		if(xpath==":parent"){
+			return this.select(this.selectedNode.parentNode);
+		}else if(xpath==":last"){
+			return this.select(this.selectedNode.lastChild);
+		}else if(xpath==":first"){
+			return this.select(this.selectedNode.firstChild);
+		}else if(xpath==":children"){
+			return this.children();
+		}else if(F.string.endWith(xpath,":children")){
+			this.select(xpath.substr(0,xpath.lastIndexOf(":")),parent);
+			return this.children();
+		}else if(F.string.endWith(xpath,":parent")){
+			this.select(xpath.substr(0,xpath.lastIndexOf(":")),parent);
+			return this.select(this.selectedNode.parentNode);
+		}else if(F.string.endWith(xpath,":last")){
+			this.select(xpath.substr(0,xpath.lastIndexOf(":")),parent);
+			return this.select(this.selectedNode.lastChild);
+		}else if(F.string.endWith(xpath,":first")){
+			this.select(xpath.substr(0,xpath.lastIndexOf(":")),parent);
+			return this.select(this.selectedNode.firstChild);
+		}else{
+			this.queue.push((parent||this.ROOT).selectSingleNode(xpath));
+		}
+	}
+	else if(typeof xpath=="number") {
+		this.selectedNode = this.queue[xpath];
+		this.index = xpath;
+		return this;
+	}
+	else this.queue.push(xpath);
+	if(this.queue[this.queue.length-1]==null){
+		this.queue.pop();
+		this.index=-1;
+		return this;
+	}
+	this.selectedNode = this.queue[this.queue.length-1];
+	this.index = this.queue.length-1;
+	return this;
+};
+MoLibXML.prototype.parent = function(){
+	this.select(":parent");
+	return this;
+};
+MoLibXML.prototype.first = function(){
+	this.select(":first");
+	return this;
+};
+MoLibXML.prototype.last = function(){
+	this.select(":last");
+	return this;
+};
+MoLibXML.prototype.children = function(filter){
+	if(this.selectedNode==null)return new MoLibXML.Collection();
+	var col = new MoLibXML.Collection();
+	var children = this.selectedNode.childNodes;
+	for(var i=0;i<children.length;i++){
+		if(filter===undefined)col.push(children[i]);
+		else{
+			var node = MoLibXML.filter(children[i],filter,i);
+			if(node!=null)col.push(node);
+		}
+	}
+	return col;
+};
+MoLibXML.prototype.nodes = function(xpath,filter){
+	if(this.selectedNode==null)return new MoLibXML.Collection();
+	var col = new MoLibXML.Collection();
+	var children = this.selectedNode.selectNodes(xpath);
+	for(var i=0;i<children.length;i++){
+		if(filter===undefined)col.push(children[i]);
+		else{
+			var node = MoLibXML.filter(children[i],filter,i);
+			if(node!=null)col.push(node);
+		}
+	}
+	return col;
+};
+MoLibXML.prototype.save = function(path){
+	if(this.DOM==null)return this;
+	this.DOM.save(path);
+	return this;
+};
+MoLibXML.prototype.root = function(){
+	this.select(this.ROOT);
+	return this;
+};
+MoLibXML.prototype.index = function(){
+	return this.index;
+};
+MoLibXML.prototype.clear = function(){
+	if(this.selectedNode==null)return this;
+	while(this.selectedNode.lastChild){
+		this.selectedNode.removeChild(this.selectedNode.lastChild);
+	}
+	return this;
+};
+MoLibXML.prototype.remove = function(){
+	if(this.selectedNode==null)return this;
+	this.selectedNode.parentNode.removeChild(this.selectedNode);
+	return this;
+};
+MoLibXML.prototype.append = function(name,value){
+	if(this.selectedNode==null)return this;
+	if(typeof name=="object"){
+		if(name.constructor == Object){
+			for(var i in name){
+				if(!name.hasOwnProperty(i))continue;
+				var b = this.DOM.createElement(i);
+				b.text = name[i];
+				this.selectedNode.appendChild(b);
+			}
+		}else{
+			F.each(name,function(key,dict,state){
+				var b = state.DOM.createElement(key);
+				b.text = dict(key);
+				state.selectedNode.appendChild(b);
+			},this);
+		}
+		return this;
+	}else if(typeof value=="object" && value.constructor == Array){
+		for(var i=0;i<value.length;i++){
+			var b = this.DOM.createElement(name);
+			b.text = value[i];
+			this.selectedNode.appendChild(b);
+		}
+		return this;
+	}
+	var e = this.DOM.createElement(name);
+	if(value!==undefined)e.text = value;
+	this.selectedNode.appendChild(e);
+	return e;
+};
+MoLibXML.prototype.appendAndSelect = function(name,value){
+	this.select(this.append(name,value));
+	return this;
+};
+MoLibXML.prototype.name = function(){
+	if(this.selectedNode==null)return this;
+	return this.selectedNode.nodeName;
+};
+MoLibXML.prototype.type = function(){
+	if(this.selectedNode==null)return this;
+	return this.selectedNode.nodeType;
+};
+MoLibXML.prototype.xml = function(){
+	if(this.selectedNode==null)return this;
+	return this.selectedNode.xml;
+};
+MoLibXML.prototype.text = function(value){
+	if(this.selectedNode==null)return this;
+	if(value!==undefined){
+		this.selectedNode.text = value;
+		return this;
+	}
+	return this.selectedNode.text;
+};
+MoLibXML.prototype.value = function(){
+	if(this.selectedNode==null)return this;
+	return this.selectedNode.nodeValue;
+};
+MoLibXML.prototype.comment = function(value){
+	if(this.selectedNode==null)return this;
+	var c = this.DOM.createComment(value);
+	this.selectedNode.appendChild(c);
+	return this;
+};
+MoLibXML.prototype.CDATA = function(value){
+	if(this.selectedNode==null)return this;
+	var c = this.DOM.createCDATASection(value);
+	this.selectedNode.appendChild(c);return this
+};
+MoLibXML.prototype.attr = function(name,value){
+	if(this.selectedNode==null)return this;
+	if(value!==undefined){
+		this.selectedNode.setAttribute(name,value);return this
+	}
+	var value1 = this.selectedNode.getAttribute(name);
+	return (value1==null?"":value1);
+};
+MoLibXML.Load = function(url,charset){
+	if(typeof url!="string")return new MoLibXML();
+	var islocal=true;
+	if(url.length>8 && url.substr(0,8).toLowerCase()=="https://")islocal=false;
+	else if(url.length>7 && url.substr(0,7).toLowerCase()=="http://")islocal=false;
+	var xml="";
+	if(islocal){
+		if(url.length>2 && url.substr(1,1)==":"){
+			var stream = new ActiveXObject("ADODB.Stream");
+			stream.Type = 2;
+			stream.Mode = 3;
+			stream.Charset = charset||"utf-8";
+			stream.Open();
+			stream.LoadFromFile(url);
+			xml = stream.ReadText();
+			stream.close();
+			stream = null;
+		}else{
+			xml = url;
+		}
+	}else{
+		var WS = new ActiveXObject("MSXML2.ServerXMLHTTP.3.0");
+		WS.open("GET",url,false);
+		WS.send();
+		if(WS.readyState==4) xml = F.string.frombinary(WS.responseBody,charset||"utf-8");
+		WS = null;
+	}
+	if(xml!=""){
+		var dom = new ActiveXObject("MSXML2.DomDocument");
+		dom.loadXML(xml);
+		return new MoLibXML(dom);
+	}else{
+		return new MoLibXML();
+	}
+};
+MoLibXML.LoadText = function(content){
+	var dom = new ActiveXObject("MSXML2.DomDocument");
+	dom.loadXML(content);
+	return new MoLibXML(dom);	
+}
+return MoLibXML;
