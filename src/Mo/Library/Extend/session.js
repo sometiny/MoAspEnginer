53c8c87ea11acb4beffbe255f74480c8e8aca448
anlige <zhanghuiguoanlige@126.com>
2014-09-03 00:24:17 +0800

    修复基于Application的Session中的bug；

--- src/Mo/Library/Extend/session.js
+++ src/Mo/Library/Extend/session.js
@@ -1,48 +1,79 @@
 ﻿if(exports.session) return exports.session;
-return exports.session = (function(){
-	var writeApplication = function(key,value)
-	{
-		Application.Lock();
-		Application(key) = value;
-		Application.UnLock();
-	};
+return exports.session = (function(driver){
 	var $session=function(key,value)
 	{
 		if(value===undefined)
 		{
-			return Application($session.id+":" + key);
+			return driver.readSession($session.id+":" + key);
 		}
-		writeApplication($session.id+":" + key, value);
+		driver.writeSession($session.id+":" + key, value);
 	};
-	$session.timeout = 20;
+	$session.setTimeout = function(timeout)
+	{
+		driver.writeSession($session.id + ".timeout",timeout);
+		$session.timeout = timeout;
+	};
+	$session.timeout = 1200;
 	$session.id = F.get("sessionid");
 	if($session.id == "")
 	{
 		$session.id = F.random.word(20);
 	}
-	if(typeof Application($session.id) == "number")
+	else
 	{
-		if(F.timespan() - Application($session.id) >$session.timeout){
-			var lists=[];
-			F.each(Application.Contents, function(q) {
-				if(q.length > $session.id.length+1 && q.substr(0,$session.id.length+1) == ($session.id + ":"))
-				{
-					lists.push(q);
-				}
-			});
-			while(lists.length>0)
-			{
-				Application.Contents.Remove(lists.pop());
-			}
-			Application.Contents.Remove($session.id);
+		var timeout = driver.readSession($session.id + ".timeout");
+		if(typeof timeout=="number") $session.timeout = timeout;
+	}
+	var time_start = driver.getStartTimeSpan($session.id);
+	if(typeof time_start == "number")
+	{
+		if(F.timespan() - time_start >$session.timeout){
+			driver.clearSession($session.id);
 			$session.id = F.random.word(20);
 		}
-		writeApplication($session.id, F.timespan());
+		driver.writeSession($session.id, F.timespan());
 	}
 	else
 	{
 		$session.id = F.random.word(20);
-		writeApplication($session.id, F.timespan());
+		driver.writeSession($session.id, F.timespan());
 	}
 	return $session;
-})();
+})((function(){
+	/*session driver, you can use your own driver, such as IO, Database and so on...*/
+	var driver = {};
+	driver.writeSession = function(key,value)
+	{
+		Application.Lock();
+		Application(key) = value;
+		Application.UnLock();
+	};
+
+	driver.readSession = function(key)
+	{
+		return Application(key);
+	};
+
+	driver.clearSession = function(sessionid)
+	{
+		var lists=[];
+		F.each(Application.Contents, function(q) {
+			var l = q.substr(0,sessionid.length+1);
+			if(q.length > sessionid.length+1 && (l == (sessionid + ":") || l == (sessionid + ".")))
+			{
+				lists.push(q);
+			}
+		});
+		while(lists.length>0)
+		{
+			Application.Contents.Remove(lists.pop());
+		}
+		Application.Contents.Remove(sessionid);
+	};
+	
+	driver.getStartTimeSpan = function(sessionid)
+	{
+		return Application(sessionid);
+	};
+	return driver;	
+})());
