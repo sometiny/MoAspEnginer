3de1e2cd34a04ec66f7b5b32ebc933c98be7dcef
Anlige <zhanghuiguoanlige@126.com>
2015-10-13 15:17:04 +0800

    优化数据库表前缀的定义；

--- src/Mo/Library/Extend/lib/model.js
+++ src/Mo/Library/Extend/lib/model.js
@@ -219,7 +219,12 @@ function __Model__(tablename,pk,cfg,tablePrex){
 	if(!Model__.connect(cfg)) return;
 	this.base = Connections[cfg];
 	this.type = this.base.cfg["DB_Type"];
-	this.tablePrex = (tablePrex || (this.base.cfg["DB_TABLE_PERX"] || Mo.Config.Global.MO_TABLE_PERX));
+	this.tablePrex = Mo.Config.Global.MO_TABLE_PERX;
+	if(typeof tablePrex == "string"){
+		this.tablePrex = tablePrex;
+	}else if(typeof this.base.cfg["DB_TABLE_PERX"] == "string") {
+		this.tablePrex = this.base.cfg["DB_TABLE_PERX"];
+	}
 	this.table = this.tablePrex + this.table;
 	this.tableWithNoSplitChar = this.table;
 	if(this.base.useCommand && this.type != "OTHER"){
